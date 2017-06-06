7b0270785b4c4404805887e5eec4f7c8138dccfb
umahmudov <umid.ma@gmail.com>
2017-06-06 13:52:26 +0400

    Try implement modular

--- single/src/main/resources/static/js/hello.js
+++ single/src/main/resources/static/js/hello.js
@@ -1,64 +1,68 @@
-    angular.module('hello', [ 'ngRoute' ])
-        .config(function($routeProvider, $httpProvider,$locationProvider,$qProvider) {
-            //  Due to aa077e8, the default hash-prefix used for $location
-            // hash-bang URLs has changed from the
-            // empty string ('') to the bang ('!').
-            //
-            //  If you actually want to have no hash-prefix,
-            // then you can restore the previous behavior by adding
-            // a configuration block to your application:
-            $locationProvider.hashPrefix('');
+angular.module('hello', ['ngRoute', 'home', 'message', 'navigation'])
+    .config(function ($routeProvider, $httpProvider, $locationProvider, $qProvider) {
+        //  Due to aa077e8, the default hash-prefix used for $location
+        // hash-bang URLs has changed from the
+        // empty string ('') to the bang ('!').
+        //
+        //  If you actually want to have no hash-prefix,
+        // then you can restore the previous behavior by adding
+        // a configuration block to your application:
+        $locationProvider.hashPrefix('');
 
 
-            //is added for "Possibly unhandled rejection"
-            $qProvider.errorOnUnhandledRejections(false);
+        //is added for "Possibly unhandled rejection"
+        $qProvider.errorOnUnhandledRejections(false);
 
 
-            $routeProvider
-                .when('/', {
-                templateUrl : 'home.html',
-                controller : 'home',
-                controllerAs: 'controller'})
-                .when('/login', {
-                templateUrl : 'login.html',
-                controller : 'navigation',
-                controllerAs: 'controller'})
-                .otherwise('/');
+        $routeProvider
+            .when('/', {
+                templateUrl : 'js/home/home.html',
+                controller: 'home'
+            })
+            .when('/message', {
+                templateUrl: 'js/message/message.html',
+                controller: 'message'
+            })
+            .when('/login', {
+                templateUrl : 'js/navigation/login.html',
+                controller: 'navigation'
+            })
+            .otherwise('/');
 
-            $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-        })
-        .controller('navigation',function($rootScope, $http, $location, $route) {
+    })
+    .controller('navigation', function ($rootScope, $http, $location, $route) {
 
-                var self = this;
+        var self = this;
 
-                self.tab = function(route) {
-                    return $route.current && route === $route.current.controller;
-                };
+        self.tab = function (route) {
+            return $route.current && route === $route.current.controller;
+        };
 
-                var authenticate = function(credentials, callback) {
+        var authenticate = function (credentials, callback) {
 
-                    var headers = credentials ? {
-                        authorization : "Basic "
-                        + btoa(credentials.username + ":"
-                            + credentials.password)
-                    } : {};
+            var headers = credentials ? {
+                authorization: "Basic "
+                + btoa(credentials.username + ":"
+                    + credentials.password)
+            } : {};
 
-                    $http.get('user', {
-                        headers : headers
-                    }).then(function(response) {
-                        if (response.data.name) {
-                            $rootScope.authenticated = true;
-                        } else {
-                            $rootScope.authenticated = false;
-                        }
-                        callback && callback($rootScope.authenticated);
-                    }, function() {
-                        $rootScope.authenticated = false;
-                        callback && callback(false);
-                    });
+            $http.get('user', {
+                headers: headers
+            }).then(function (response) {
+                if (response.data.name) {
+                    $rootScope.authenticated = true;
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback($rootScope.authenticated);
+            }, function () {
+                $rootScope.authenticated = false;
+                callback && callback(false);
+            });
 
-                };
+        };
 
 //function is called when the controller is loaded to see if the user is actually
 // already authenticated (e.g. if he had refreshed the browser in the middle of a session).
@@ -66,45 +70,45 @@
 // authentication is done by the server, and we don’t want to trust the browser
 // to keep track of it.
 
-                authenticate();
+        authenticate();
 
-                self.credentials = {};
-                self.login = function() {
-                    authenticate(self.credentials, function(authenticated) {
-                        if (authenticated) {
-                            console.log("Login succeeded")
-                            $location.path("/");
-                            self.error = false;
-                            $rootScope.authenticated = true;
-                        } else {
-                            console.log("Login failed")
-                            $location.path("/login");
-                            self.error = true;
-                            $rootScope.authenticated = false;
-                        }
-                    })
-                };
-
-                self.logout = function() {
-                    $http.post('logout', {}).finally(function() {
-                        $rootScope.authenticated = false;
-                        $location.path("/");
-                    });
+        self.credentials = {};
+        self.login = function () {
+            authenticate(self.credentials, function (authenticated) {
+                if (authenticated) {
+                    console.log("Login succeeded")
+                    $location.path("/");
+                    self.error = false;
+                    $rootScope.authenticated = true;
+                } else {
+                    console.log("Login failed")
+                    $location.path("/login");
+                    self.error = true;
+                    $rootScope.authenticated = false;
                 }
+            })
+        };
 
-            })
-        .controller('home', function($http) {
-            var self = this;
-            $http.get('token').then(function(response) {
-                console.log(response.data.token);
-                $http({
-                    url : 'http://localhost:9000',
-                    method : 'GET',
-                    headers : {
-                        'X-Auth-Token' : response.data.token
-                    }
-                }).then(function(response) {
-                    self.greeting = response.data;
-                });
-            })
-        });
+        self.logout = function () {
+            $http.post('logout', {}).finally(function () {
+                $rootScope.authenticated = false;
+                $location.path("/");
+            });
+        }
+
+    })
+    .controller('home', function ($http) {
+        var self = this;
+        $http.get('token').then(function (response) {
+            console.log(response.data.token);
+            $http({
+                url: 'http://localhost:9000',
+                method: 'GET',
+                headers: {
+                    'X-Auth-Token': response.data.token
+                }
+            }).then(function (response) {
+                self.greeting = response.data;
+            });
+        })
+    });
